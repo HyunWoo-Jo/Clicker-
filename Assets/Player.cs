10b74ad64c78c3613e45b5e38ef989859d1a86a1
HyunWooJo <CoreHyunWoo@gmail.com>
2020-10-23 13:23:53 +0900

    Clicker 05
    프로젝트 초기 구상
    Player
    Upgrade
    Gold
    추가
    
    관련내용
    https://jogamedev.tistory.com/6

---
+++ Assets/Player.cs
@@ -0,0 +1,9 @@
+﻿public class Player : SingletonObject<Player> // SingletonObject를 상속
+{
+    public Gold gold = new Gold();
+    public Gold increaseGold; // 클릭시 증가 골드
+
+    public void ClickAct() { // 클릭시 골드를 추가한다
+        gold.SetGold(increaseGold.GetGold() + gold.GetGold());
+    }
+}
---
+++ Assets/Upgrade.cs
@@ -0,0 +1,8 @@
+﻿public class Upgrade {
+    public int currentUpgrade = 0; //현재 업그레이드
+    public Gold upgradePrice = new Gold(); //업그레이드 비용
+
+    public void TryUpgrade() { // 업그레이드 시도
+
+    }
+}
