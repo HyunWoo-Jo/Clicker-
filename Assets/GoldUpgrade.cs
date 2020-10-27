f6f99f8030cc325f40006f20c6c977f0d5be64c7
HyunWooJo <CoreHyunWoo@gmail.com>
2020-10-27 09:02:42 +0900

    09.업그레이드 추가
    alter
    -IncreaseGold.cs
    -GoldUpgrade.cs

--- Assets/GoldUpgrade.cs
+++ Assets/GoldUpgrade.cs
@@ -5,7 +5,7 @@ public abstract class GoldUpgrade : MonoBehaviour, IUpgradeAble
     protected Gold price; // 업그레이드 비용
     protected Gold increase; // 업그레이드 증가량
 
-    public RenewText priceText; // 비용 Text
+    public RenewText priceText; // 비용 Text 
     public RenewText increaseText; // 증가량 Text
 
     public abstract void TryUpgrade(); // abstract로 반드시 자식 클레스에서 인터페이스 구현 하도록한다.
@@ -15,6 +15,6 @@ public abstract class GoldUpgrade : MonoBehaviour, IUpgradeAble
 
     protected virtual void RenewText() {
         priceText.Renew(price.GetGold().ToString()); // priceText를 갱신한다.
-        increaseText.Renew(price.GetGold().ToString()); // increaseText를 갱신하다.
+        increaseText.Renew(increase.GetGold().ToString()); // increaseText를 갱신하다.
     }
 }
